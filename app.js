4a99a4e5c70126895c5e028905c18d38329cb828
kamal-singh <kamal000rawat@gmail.com>
2021-09-28 13:49:34 +0530

    final commit

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('19dffc7698384bba9b3b3eee5566c0dc');
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI("19dffc7698384bba9b3b3eee5566c0dc");
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 let newsdata = "";
 let ct = "";
 let search = "";
 let detail;
-app.set("view engine","ejs");
-app.use(express.static("public"))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/",function(req,res){
-  newsapi.v2.topHeadlines({
-    category: ct,
-    q: search,
-    language: 'en',
-    country: 'in'
-  }).then(response => {
+app.get("/", function (req, res) {
+  newsapi.v2
+    .topHeadlines({
+      category: ct,
+      q: search,
+      language: "en",
+      country: "in",
+    })
+    .then((response) => {
       newsdata = response;
-      res.render("news",{newsdata:newsdata.articles,cate:ct,totalresult:newsdata.totalResults});
-  });
+      res.render("news", {
+        newsdata: newsdata.articles,
+        cate: ct,
+        totalresult: newsdata.totalResults,
+      });
+    });
 });
-app.get("/about",function(req,res){
+app.get("/about", function (req, res) {
   res.render("about");
-})
-app.get("/contact",function(req,res){
+});
+app.get("/contact", function (req, res) {
   res.render("contact");
-})
-app.post("/",function(req,res){
+});
+app.post("/", function (req, res) {
   ct = req.body.submit;
   search = req.body.search;
   res.redirect("/");
-})
-app.listen(process.env.PORT || 3000,function(){
+});
+app.listen(process.env.PORT || 3000, function () {
   console.log("START SERVER\nPORT : 3000");
-})
+});
